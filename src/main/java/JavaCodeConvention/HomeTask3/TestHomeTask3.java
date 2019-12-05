44bf040cc191b707ba5cf25823e8c84d91544285
helenjon <helena.mokina@gmail.com>
2019-12-05 15:27:28 +0200

    Java Code Convention. OOP > Home Task 3

---
+++ src/main/java/JavaCodeConvention/HomeTask3/TestHomeTask3.java
@@ -0,0 +1,29 @@
+package JavaCodeConvention.HomeTask3;
+
+
+import org.junit.Test;
+
+public class TestHomeTask3 {
+
+
+//    "1. Create a class that describes a triangle. It should be defined by using three points which describe the vertexes of this
+//    triangle. Create a method that is able to calculate the are of the triangle: it should return a double value (an area) and should
+//    throw and exception if the area could not be calculated (for example if the triangle is self-intersecting).
+//    Create a simple class which will use the triangle class.
+//    Note: the exception should be Checked"
+
+
+    public static void main(String [] args){
+        Triangle test1 = new Triangle(1,1,2,5,5,3);
+        test1.calculateSquare();
+        Triangle test2 = new Triangle();
+        test2.setPoints("A", 1,1);
+        test2.calculateSquare();
+        test2.setPoints("M", 5,3);
+        test2.setPoints("B", 2,5);
+        test2.calculateSquare();
+
+    }
+}
+
+
---
+++ src/main/java/JavaCodeConvention/HomeTask3/Triangle.java
@@ -0,0 +1,59 @@
+package JavaCodeConvention.HomeTask3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Triangle {
+
+    private  List<Integer> A = new ArrayList<>();
+    private  List<Integer> B = new ArrayList<>();
+    private  List<Integer> C = new ArrayList<>();
+
+    public Triangle(Integer x1, Integer y1, Integer x2, Integer y2, Integer x3, Integer y3){
+        this.A.add(x1);
+        this.A.add(y1);
+        this.B.add(x2);
+        this.B.add(y2);
+        this.C.add(x3);
+        this.C.add(y3);
+    }
+
+    public Triangle(){ }
+
+
+    void setPoints(String side, Integer x, Integer y) {
+        try {
+            switch (side) {
+                case "C":
+                    this.C.add(x);
+                    this.C.add(y);
+                    break;
+                case "B":
+                    this.B.add(x);
+                    this.B.add(y);
+                    break;
+                case "A":
+                    this.A.add(x);
+                    this.A.add(y);
+                default: System.out.println("There is NO " +side +" Please enter correct side");
+            }
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    }
+
+    void calculateSquare(){
+        Double s;
+        try {
+            s = Math.abs((((A.get(0) - C.get(0)) * (A.get(1) - C.get(1))) - ((B.get(0) - C.get(0)) * (B.get(1) - C.get(1))))) * 0.5;
+            System.out.println(s);
+        }
+        catch (IndexOutOfBoundsException e){
+            if (A.size()<2)  System.out.println("setup XY for A" );
+            if (B.size()<2)  System.out.println("setup XY for B" );
+            if (C.size()<2)  System.out.println("setup XY for C" );
+        }
+
+    }
+
+}
