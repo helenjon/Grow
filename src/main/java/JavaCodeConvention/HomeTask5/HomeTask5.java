c6af228c25821156fe654e5b42653e55648cfefa
helenjon <helena.mokina@gmail.com>
2019-12-07 16:43:02 +0200

    Java Code Convention. OOP > Home Task 5

---
+++ src/main/java/JavaCodeConvention/HomeTask5/HomeTask5.java
@@ -0,0 +1,50 @@
+package JavaCodeConvention.HomeTask5;
+
+public class HomeTask5 {
+// "1. Implement an RLE data compression for a given string. RLE produces sequences in which the same data values occurs " +
+//         "in data elements are stored as a single data value and a counter. For example, AAABB => 3A2B. If you have a " +
+//         "number in the input string then it should be escaped using \ (slash) if you have a \ in the input string it " +
+//         "should be escaped by another \. For example:
+//    Input string: AAAAaaaBBBBBB\12
+//    Output string: 4A3a6B\\\1\2
+//    Input string: AaaaBB2
+//    Output string: 1A3a2B\2
+//    Input string: AaaaBB\\2
+//    Output string: 1A3a2B\\\\\2"
+
+
+    public static void main(String [] args) {
+
+      String s =  "AaaaBB2";
+      Integer counter = 0;
+      String st= "";
+      String rez= "";
+
+      // add / to numbers
+        for (String ss : s.split("")) {
+           if (ss.matches("[0-9<>%$/]")) {
+               st = st + "//"+ss;
+           }
+           else st = st + ss;
+        }
+       System.out.println(st);
+        {
+            for (int i = 0; i < st.length(); i++) {
+               // Count occurrences of current character
+                int count = 1;
+                    while (i < st.length() - 1 && st.charAt(i) == st.charAt(i + 1)) {
+                        count++;
+                        i++;
+                    }
+
+                if (new StringBuilder().append(st.charAt(i)).toString().matches("[0-9,<>%$/]")){
+                        // warning for compiler -  Warning:(41, 35) String concatenation '+' in loop  - why it is not good?
+                        rez = rez + st.charAt(i);
+                }
+                else {
+                rez = rez + count + st.charAt(i);}
+            }
+        }
+        System.out.print(rez);
+    }
+}
