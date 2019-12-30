32b1bc8bdf8984d24873307c48386357d29c5328
helenjon <helena.mokina@gmail.com>
2019-12-30 14:43:17 +0200

    refactoring - aligning with GROW structure > HomeTask 12 and 8

---
+++ src/main/java/JavaBasics/HomeTask12/HomeTask12.java
@@ -0,0 +1,31 @@
+package JavaBasics.HomeTask12;
+
+//Самостоятельно разберитесь с классом(классами) в Java, позволяющими читать
+//        символьные данные из файла. Создайте файл, содержащий информацию о книгах
+//        из библиотеки, прочитайте его, и на основе прочитанной информации создайте
+//        коллекцию книг.
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HomeTask12 {
+
+    public static void main (String[] args) throws IOException {
+
+        String fromFile;
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\My doc\\Idea_projects\\grow\\Lesson12_HT.txt"));
+        while((fromFile = reader.readLine()) != null ){
+            if(!fromFile.isEmpty()){
+                System.out.println(fromFile);
+            }
+        }
+
+
+    }
+
+
+
+}
+
